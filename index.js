9ddfebd7b7cabebfd4903b930e390854008ba11b
mohammad-Zaid-ansari <67639424+mohammad-Zaid-ansari@users.noreply.github.com>
2020-07-23 07:52:51 +0530

    Multiple Button Funtionality

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ let canvas = document.getElementById('ctx')
 let ctx = canvas.getContext("2d")
 const width = canvas.width = window.innerWidth
 const height = canvas.height = window.innerHeight
-// ctx.fillStyle = 'red'
-// ctx.fillRect(0,0,150,200)
 
+let keyObj = {}
 
 
 // Ball Class
@@ -67,11 +66,11 @@ class Ball {
 }
 
 class playBall extends Ball {
-    constructor(size, x, y, Vx, Vy,color) {
-        super(size, x, y, Vx, Vy,color)
+    constructor(size, x, y, Vx, Vy, color) {
+        super(size, x, y, Vx, Vy, color)
     }
 
-    
+
 
     draw() {
         ctx.beginPath()
@@ -88,8 +87,8 @@ class playBall extends Ball {
                 const distance = Math.sqrt(dx * dx + dy * dy) //pythagoras Theorem
 
                 if (distance < this.size + arr[z].size) {
-                    arr.splice(z-1,z)
-                    
+                    arr.splice(z - 1, z)
+
                 }
             }
 
@@ -97,33 +96,50 @@ class playBall extends Ball {
 
     }
 
-    update(){
-        if ((this.x +this.size)<= 0) {
-            this.x = width-10
-        }else if ((this.x +this.size)>= width) {
+    update() {
+        if ((this.x + this.size) <= 0) {
+            this.x = width
+        } else if ((this.x + this.size) >= width) {
             this.x = 0
         }
-        if ((this.y + this.size)<= 0) {
-            this.y = height-10
-        }else if ((this.y + this.size)>= height) {
+        if ((this.y + this.size) <= 0) {
+            this.y = height
+        } else if ((this.y + this.size) >= height) {
             this.y = 0
+        }
+    }
+
+    keysFuntionality(e) {
+        // Adding a event listener to window for key funtionality
+        window.onkeydown = onkeyup = (e) => {
+            keyObj[e.keyCode] = e.type == 'keydown'
+            this.update()
+
+            // For keys "A" & "W"
+            if (keyObj[65] && keyObj[87]) {
+                this.x += -(13)
+                this.y += -(13)
+            } else if (keyObj[65]) {
+                this.x += -(13)
+            } else if (keyObj[87]) {
+                this.y += -(13)
+            }
+
+            // For keys "S" & "D"
+
+            if (keyObj[83] && keyObj[68]) {
+                this.x += 13
+                this.y += 13
+            } else if (keyObj[68]) {
+                this.x += 13
+            } else if (keyObj[83]) {
+                this.y += 13
+            }
         }
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// A funtion to get a random number in a given min and max range
 let num1 = (max, min) => {
     let num = Math.floor(Math.random() * (max - min + 1)) + min
     return num
@@ -133,7 +149,7 @@ let arr = []
 
 
 while (arr.length < 20) {
-    let size = num1(7, 5)
+    let size = num1(7, 5);
 
     let ball = new Ball(size,
         num1(width - size, 0 + size),
@@ -141,29 +157,17 @@ while (arr.length < 20) {
         num1(14, 7),
         num1(14, 7),
         "rgb(" + num1(255, 0) + "," + num1(255, 0) + "," + num1(255, 0) + ")")
-    arr.push(ball)
+    arr.push(ball);
 }
 
-let playBall1 =new playBall(10, num1(width - num1(13, 8), 0 + num1(13, 8)),
-    num1(height - num1(13, 8), 0 + num1(13, 8)), 7, 8,'red')
-console.log(playBall1)
-// arr.forEach((element,i) => {
-//     element.draw()
+let playBall1 = new playBall(10, num1(width - num1(13, 8), 0 + num1(13, 8)),
+    num1(height - num1(13, 8), 0 + num1(13, 8)), 7, 8, 'red');
 
 
-// });
-
-// ctx.arc(10,50,3,0,2*Math.PI)
-// ctx.fillStyle = "red"
-// ctx.fill()
-
-// arr[0].draw()
-
-
-// Function For Moving Balls
-
+// Recursively Calling move() with requestAnimationFrame()
 function move() {
-    ctx.fillStyle = "rgba(0,0,0,0.2)" //this fillstyle and fillRect erase the path traced by balls as they are also called recurrently
+    ctx.fillStyle = "rgba(0,0,0,0.2)"
+    //this fillstyle and fillRect erase the path traced by balls as they are also called repeatedly
     ctx.fillRect(0, 0, width, height)
 
     arr.forEach((element, i) => {
@@ -172,56 +176,11 @@ function move() {
         element.collision()
 
     });
-    // debugger
-    window.onkeydown = (e) => {
-        console.log(e.key);
-        let _this = playBall1        
-        _this.update()
 
-        switch (e.key) {
-            
-            case "w":
-                _this.y += -(13)
-                break;
-
-            case "s":
-                _this.y += (13) 
-                break;
-
-            case "d":
-                _this.x += (13)
-                break;
-
-            case "a":
-                _this.x += -(13)
-                break;
-
-            // default:
-            //     console.log();
-            //     _this.x = 5
-            //     _this.y = 5
-            //     break;
-        }
-        
-
-        
-    }
-    playBall1.draw()
-    // playBall1.update()
-    playBall1.collision()
-    // playBall1.x += 5
-    //     playBall1.y += 5
-    
-
-    // for (let i = 0; i < arr.length; i++) {
-    //     arr[i].draw()
-    //     arr[i].update()
-    //     arr[i].collision()
-
-    // }
-
-
+    playBall1.draw();
+    playBall1.keysFuntionality()
+    playBall1.collision();
     requestAnimationFrame(move)
-
 }
+
 move()
